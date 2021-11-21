72f1a17ea2675cce47390c39de6f516947bd2307
Joshua-Barawa <joshua.barawa@student.moringaschool.com>
2021-11-21 12:31:41 +0300

    added jquery functions to cards in whatwedo section

---
+++ index.js
@@ -0,0 +1,21 @@
+
+$(document).ready( function(){
+
+$('.card1').click(function(){
+    $('#description1').toggle();
+    $('#img1').toggle();
+})
+
+$('.card2').click(function(){
+    $('#description2').toggle();
+    $('#img2').toggle();
+})
+
+$('.card3').click(function(){
+    $('#description3').toggle();
+    $('#img3').toggle();
+})
+
+
+
+})
